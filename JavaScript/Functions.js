5d10523d607498b88aeecebb2287a27caba36ebf
Warnedluis <avilaramirezluiseduardo1@gmail.com>
2024-02-09 12:06:56 -0600

    Hicimos un formulario y tambien agregamos botones con funciones nuevas

--- JavaScript/Functions.js
+++ JavaScript/Functions.js
@@ -1,4 +1,8 @@
 function changeColor(){
     document.getElementById("Documento").style.backgroundColor="white";
     document.getElementById("Documento").style.color="Orange";
+    document.getElementById("Documento").textContent="Hola";
+}
+function enviarMsg (){
+    alert("Hola amigo");
 }
